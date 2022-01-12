5749246bfe0113daaa7c9fb6db0adb31a8fc3115
ReneCarneiro <renemelosjc@gmail.com>
2022-01-12 17:48:55 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ return (
     <nav className="container">
         <div className="dsmovie-nav-content">
                        <h1>DSMovie</h1>
-            <a href="https://github.com/ReneCarneiro">
+            <a href="https://github.com/ReneCarneiro" target="_blank" rel="noreferrer"> 
                 <div className="dsmovie-contact-container">
                     <GithubIcon />
                     <p className="dsmovie-contact-link">/devsuperior</p>
