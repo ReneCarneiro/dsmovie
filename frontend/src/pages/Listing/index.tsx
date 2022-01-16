82e419c6971c10409521456fb37c95b730dbc91a
ReneCarneiro <renemelosjc@gmail.com>
2022-01-16 20:32:52 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
+
 
 function Listing() {
+//errado
+axios.get(`${BASE_URL}/movie?size=12&page=0`)
+.then(response => {
+console.log(response.data);
+});
 
     return (
         <>
